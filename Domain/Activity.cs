58136d9568c956b0d4c44a4cf0dc5fc473bbd71e
Mikail Miller <65094447+ironlionm4n@users.noreply.github.com>
2024-03-12 13:10:59 -0400

    init commit

---
+++ Domain/Activity.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain;
+
+// Entity Framework Model or Entity, used for a record in the database
+// Each property will be a column in the database
+public class Activity
+{
+    // Specifically Id because Entity Framework will look for a property with this name
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public DateTime Date { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public string City { get; set; }
+    public string Venue { get; set; }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Domain.Activity> Activities { get; set; }
+}
