8c882610fae914b2dc66ab158d43e261b71fd13f
Mikail Miller <65094447+ironlionm4n@users.noreply.github.com>
2024-03-13 09:27:25 -0400

    update and delete endpoints, services extension method

---
+++ API/ExtensionMethods/ApplicationServiceExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Application.Activities;
+using Application.Core;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.ExtensionMethods;
+
+public static class ApplicationServiceExtensions
+{
+    // this refers to where this method is being called, in this case, the Program.cs file
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddDbContext<DataContext>(option =>
+        {
+            option.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            });
+        });
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
+        services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+        return services;
+    }
+}
